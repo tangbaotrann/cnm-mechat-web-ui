f8d33aee5e53ad87797e0ae3ef96131ce8940ca2
Admins <vothanhnho8005@gmail.com>
2022-09-15 20:19:53 +0700

    #2 - login/register

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 // libs
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+  
+import {Switch, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // me
 import Home from './pages/Home';
 import Directory from './pages/Directory';
-
+//me too
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import ConfirmOTP from './pages/Register/ConfirmOTP/ConfirmOTP';
 function App() {
     return (
+      
         <Router>
+              
             <Routes>
-                {/* Home page */}
+               {/* Home page
                 <Route exact path="/" element={<Home />} />
 
-                {/* Directory page */}
-                <Route path="/directory" element={<Directory />} />
+                {/* Directory page *
+                <Route path="/directory" element={<Directory />} />  */}
+                {/* Login */}
+                <Route exact path='/' element={<Login/>} />
+                {/* Register */}
+                <Route exact path='/Register' element={<Register/>} />
+                {/* ConfirmOTP */}
+             {/* <Route exact path='/ConfirmOTP' element={<ConfirmOTP/>} />  */}
             </Routes>
+           
         </Router>
+       
+    
     );
 }
 
