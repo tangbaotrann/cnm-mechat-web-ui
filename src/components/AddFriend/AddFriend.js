3396d387c6bdc4158ff7d57a4cea3d2c7beeba83
Admins <vothanhnho8005@gmail.com>
2022-10-01 16:57:56 +0700

    #11 call api login, save localStorage

--- src/components/AddFriend/AddFriend.js
+++ src/components/AddFriend/AddFriend.js
@@ -1,33 +1,27 @@
 import classNames from 'classnames/bind';
 import { useState, useEffect, useRef } from 'react';
-import { faXmark,faCircleXmark} from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './AddFriend.module.scss';
 import Conversation from '../Conversation';
 const cx = classNames.bind(styles);
 function AddFriend()
 {
-    const [openInfoAccount, setOpenInfoAccount] = useState(false);
+ 
     const [searchPhone, setSearchPhone] = useState('');
     const [loading, setLoading] = useState(false);
     const [showResult, setShowResult] = useState(false);
     const [searchResult, setSearchResult] = useState([]);
     const searchRef = useRef();
     // Handle open/ close model info account
-    const handleModelOpenInfoAccount = () => {
-        setOpenInfoAccount(true);
-    };
-    const handleModelCloseInfoAccount = () => {
-        setOpenInfoAccount(false);
-    };
+
     useEffect(() => {
         if (!searchPhone.trim()) {
             return;
         }
-        setLoading(true);
+   
         // fetch api
-        setSearchResult([1]);
-        setLoading(false);
+ 
     }, [searchPhone]);
     const handleChangePhone = (e) => {
         const value = e.target.value;
@@ -41,17 +35,7 @@ function AddFriend()
         searchRef.current.focus();
     };
     return (
-              <div className={cx('model-add-friend-bg')} >
-                    <div className={cx('add-friend-title')}>
-                            <span className={cx('friend-title')}>Thêm bạn</span>
-                            <button className={cx('close-btn')}>
-                                <FontAwesomeIcon
-                                    className={cx('friend-close-ic')}
-                                    icon={faXmark}
-                                    onClick={handleModelCloseInfoAccount}
-                                />
-                            </button>
-                        </div>
+            <div>
                         <div className={cx('add-phoneNumber')}>
                                <input
                                      type="tel"  
@@ -78,7 +62,7 @@ function AddFriend()
                                 <button className={cx('search')}>Tìm kiếm</button>
                             </div>            
                         </div>
-                    </div>
+                   </div>
                  
     );
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,88 +3,117 @@ import classNames from 'classnames/bind';
 import images from '~/assets/images';
 
 import { useState, useEffect } from 'react';
-import { PhoneIphone, Lock ,ArrowLeft} from '@material-ui/icons';
-import {  Link } from 'react-router-dom';
+import { PhoneIphone, Lock } from '@material-ui/icons';
+import { Link, useNavigate } from 'react-router-dom';
+//
+
 import { authentication } from '~/util/firebase';
-import {RecaptchaVerifier,signInWithPhoneNumber} from "firebase/auth";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+
+
 const cx = classNames.bind(styles);
+
+
 function Login() {
     useEffect(() => {
         document.title = 'Trang đăng nhập';
     });
-    const countryCode="+84"
-    const [phoneNumber,setPhoneNumber] = useState(countryCode);
-    const [OTP,setOTP] = useState('');
-    const [changeForm,setchangeForm] = useState(false);
-    const generateRecaptcha= ()=>{
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(localStorage.getItem("user_login"))
+        {
+            navigate('/me.chat') 
+        }
+    });
+    const sign = () => {
+        return fetch("http://localhost:5001/api/v1/users/login", {
+            method: "POST",
+            headers: {
+                "Accept": 'application/json',
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                'phoneNumber': phoneNumber,
+                'passWord': password,
+            })          
+        }).then(res => res.json())
+            .then(resData => {
+                if (resData.status === "success") {
+                    console.log(resData);
+                    //  resolve(resData);
+                    localStorage.setItem("user_login", JSON.stringify(resData))
+                    return resData;
+                    // navigate('/ConFirmOTP')   
+                }
+                else {
+                    // throw new Error("404 else");
+                    return Promise.reject(new Error("404 else"));
+                }
+            }).catch(err => {
+
+                return Promise.reject(new Error("404 else"));
+
+            })
+
+    }
+
+    const generateRecaptcha = () => {
         window.recaptchaVerifier = new RecaptchaVerifier('tam', {
-          'size': 'invisible',
-          'callback': (response) => {
-          }
+            'size': 'invisible',
+            'callback': (response) => {
+            }
         }, authentication);
     }
-    const handleSubmit = (e) => {
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log("da click");
-        console.log(phoneNumber+"ngoai if")
-        if(phoneNumber.length>=10)
-        {
-      //  setchangeForm(true);
-        console.log(phoneNumber+"trong if")
-        generateRecaptcha();
-        const appVerifier=window.recaptchaVerifier;
-        signInWithPhoneNumber( authentication,phoneNumber, appVerifier)
-        .then((confirmationResult) => {
-          // SMS sent. Prompt user to type the code from the message, then sign the
-          // user in with confirmationResult.confirm(code).
-          window.confirmationResult = confirmationResult;
-          console.log("ĐÃ gửi OTP");
-          setchangeForm(true);
-          // ...
-        }).catch((error) => {
-          // Error; SMS not sent
-          // ...
-          alert("Số điện thoại chưa đăng ký tài khoảng")
-          console.log("Chưa gửi về OTP"+error);
-        });
-        }
+        sign().then(v => {
+            console.log(v);
+            console.log("da click");
+            console.log(phoneNumber + "ngoai if")
+            if (phoneNumber.length >= 10) {
+                //  setchangeForm(true);
+                console.log(phoneNumber + "trong if")
+                generateRecaptcha();
+                const phoneNumbers = "+84" + phoneNumber.slice(1);
+                console.log(phoneNumbers + "sao khi +84")
+                const appVerifier = window.recaptchaVerifier;
+                 signInWithPhoneNumber(authentication, phoneNumbers, appVerifier)
+                    .then((confirmationResult) => {
+                        // SMS sent. Prompt user to type the code from the message, then sign the
+                        // user in with confirmationResult.confirm(code).
+                        window.confirmationResult = confirmationResult;
+                        console.log("ĐÃ gửi OTP");
+                        // ...
+                          navigate('/ConFirmOTP')      
+    
+                    }).catch((error) => {
+                        // Error; SMS not sent
+                        // ...
+                        alert("Số điện thoại chưa đăng ký tài khoảng")
+                        console.log("Chưa gửi về OTP" + error);
+                    });
+            }
+        }).catch(err => {
+            console.log(err + "handleSubmit");
+            alert("tài khoản không tồn tại");
+        })
     };
-    const handleconfirm = (e)=>
-    {
-        if(OTP.length===6)
-        {
-          let confirmationResult =  window.confirmationResult;
-          confirmationResult.confirm(OTP).then((result) => {
-            // User signed in successfully.
-            const user = result.user;
-            // ...
-            console.log("hoan thanh")
-          }).catch((error) => {
-            // User couldn't sign in (bad verification code?)
-            // ...
-            console.log(error);
-          });
-        }
-    }
+
     return (
         <div className={cx('wrapper')}>
             <div className={cx('logo')}>
                 <img className={cx('logo-image')} src={images.logo} alt="" />
             </div>
-
-            {changeForm ===false ? 
-                <>
             <div className={cx('login-title')}>
                 <h1>Đăng Nhập</h1>
             </div>
-            </> : 
-            <div className={cx('login-title')}>
-                <h1>Xác nhận mã OTP</h1>
-            </div>}
-            {changeForm ===false ? 
-                <>
             <div className={cx('login-form')}>
                 <form onSubmit={handleSubmit}>
+
                     <div className={cx('form-phoneNumber')}>
                         <PhoneIphone className={cx('item')} />
                         <input
@@ -92,25 +121,25 @@ function Login() {
                             placeholder="Số điện thoại"
                             name="phoneNumber"
                             value={phoneNumber}
-                            onChange={e=>setPhoneNumber(e.target.value)}
-                            
+                            onChange={e => setPhoneNumber(e.target.value)}
                         />
                     </div>
                     <div className={cx('form-password')}>
                         <Lock className={cx('password-item')} />
-                        <input 
-                            type="password" 
-                            placeholder="Mật khẩu" 
-                            name="password"  
+                        <input
+                            type="password"
+                            placeholder="Mật khẩu"
+                            name="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
                         />
                     </div>
                     <div className={cx('form-button')}>
-                       
-                            <button type="submit" variant="contained" color="primary">
-                                ĐĂNG NHẬP
-                            </button>
-                            <div id="tam"></div>
-                         <Link to="/Home" ></Link>
+
+                        <button type="submit" variant="contained" color="primary" onClick={sign}>
+                            ĐĂNG NHẬP
+                        </button>
+                        <div id="tam"></div>
                     </div>
                     <div className={cx('form-forget')}>
                         <a href="/forgotpassword">Quên mật khẩu?</a>
@@ -119,39 +148,12 @@ function Login() {
             </div>
             <div className={cx('form-register')}>
                 <h1>
-                    <a>Bạn chưa có tài khoản?</a>
-                    <Link to="/Register" className={cx('form-register-register')}>
+                    <p>Bạn chưa có tài khoản? </p>
+                    <Link to="/Register" className={cx('form-register-register')} >
                         Đăng ký ngay
                     </Link>
                 </h1>
             </div>
-            </>
-                :
-                null}
-            {changeForm ===true ? 
-             <>  
-                <div className={cx('otp-form')}>
-                    <form  onSubmit={handleconfirm}> 
-                        <div className={cx('form-otp')}>    
-                        <input  
-                        type="tel"  
-                        placeholder='Nhập mã OTP'
-                        value={OTP}
-                        onChange={e=>setOTP(e.target.value)}
-                        name="otp"
-                        />
-                </div>    
-                        <div className={cx('form-buttonotp')}>
-                        <h5 className={cx('form-resend-code')}>Gửi lại mã</h5>
-                            <button type="submit" variant="contained" color="primary">Xác nhận</button>
-                        </div>
-                        <div className={cx('form-back')}>
-                        <ArrowLeft className={cx('item')}/> <Link to="/Login" className={cx('back')}>Quay lại</Link>
-                         </div> 
-                    </form>
-
-                </div> 
-            </>:null}
         </div>
     );
 }
